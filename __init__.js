ae93af9c765fcf9c0193c7c3df4b4e39f01e73ab
liam-gen <74251382+liam-gen@users.noreply.github.com>
2021-05-08 12:53:47 +0200

    Create __init__.js

---
+++ __init__.js
@@ -0,0 +1,34 @@
+// NE PAS TOUCHER !
+
+const discord = require('discord.js')
+const client = new discord.Client()
+
+let commands = { 'discordBSM': 'https://github.com/liamgen/discordBSM.js' }
+
+client.on('message', message => {
+    for (message.content.split(" ")[0] in Object.keys(commands)){
+        code = commands[message.content.split(" ")[0]]
+        message.channel.send(code)
+    }
+})
+function newCommand(name, code){
+    commands[name] = code
+}
+
+function setStatus(name){
+    client.on('ready', () => {
+        status = name.replace('$servers', client.guilds.cache.size)
+        client.user.setActivity(`${status}`)
+    })
+}
+
+function createBot(bot_token){
+    client.on('ready', () => {
+        console.log(`Logged has ${client.user.tag}`)
+    })
+    client.login(bot_token)
+}
+
+module.exports.createBot = createBot
+module.exports.setStatus = setStatus
+module.exports.newCommand = newCommand
